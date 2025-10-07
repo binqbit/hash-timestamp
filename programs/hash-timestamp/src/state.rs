e8a5b7a96c7ddf2b41092e35c2c785b36f3ca2fe
binqbit <128422584+binqbit@users.noreply.github.com>
2025-10-07 11:10:50 +0300

    Refactor state.rs to introduce PreviousBlock struct and update HashAccount and VoteInfo to use hash_id

--- programs/hash-timestamp/src/state.rs
+++ programs/hash-timestamp/src/state.rs
@@ -1,7 +1,15 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::hash::hashv;
+
+use crate::ErrorCode;
 
 // Space helpers
+pub const PREVIOUS_BLOCK_SIZE: usize = 32 /*hash_id*/
+    + 8  /*created_at*/
+    + 8; /*generation*/
+
 pub const HASH_ACCOUNT_SPACE: usize = 8 /*disc*/
+    + PREVIOUS_BLOCK_SIZE
     + 32 /*hash*/
     + 8  /*voters*/
     + 8  /*created_at*/
@@ -10,13 +18,21 @@ pub const HASH_ACCOUNT_SPACE: usize = 8 /*disc*/
 
 pub const VOTE_INFO_SPACE: usize = 8 /*disc*/
     + 32 /*voter*/
-    + 32 /*hash*/
+    + 32 /*hash_id*/
     + 8  /*amount*/
     + 1  /*bump*/
     + 7; /*padding*/
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Default, Debug)]
+pub struct PreviousBlock {
+    pub hash_id: [u8; 32],
+    pub created_at: i64,
+    pub generation: u64,
+}
+
 #[account]
 pub struct HashAccount {
+    pub previous: PreviousBlock,
     pub hash: [u8; 32],
     pub voters: u64,
     pub created_at: i64,
@@ -26,7 +42,7 @@ pub struct HashAccount {
 #[account]
 pub struct VoteInfo {
     pub voter: Pubkey,
-    pub hash: [u8; 32],
+    pub hash_id: [u8; 32],
     pub amount: u64,
     pub bump: u8,
 }
